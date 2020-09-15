2bef172147e0d071a80cfd61667f758ede00a660
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-09-15 15:59:54 -0400

    cannot figure out my timer, working on this

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-//
+
+var displayQuestionEl = $(".display-questions");
+
+var timerEl = $(".timer");
+
+var resultsEl = $(".results");
+
+var mainDisplay = document.createElement("h3");
+
+var buttonEl = document.createElement("button");
+
+var timer = 75;
+
+var index = 0;
+
+function openingPage() {
+    mainDisplay.textContent = ("Press the button to start!")
+
+    buttonEl.textContent = ("Start Quiz")
+
+    $(displayQuestionEl).append(mainDisplay, buttonEl)
+}
+
+function startQuiz() {
+
+    showTimer();
+
+}
+
+function showTimer() {
+
+    timerEl.textContent = timer;
+
+
+}
+
+function nextQuestion() {
+
+}
+
+function checkAnswer() {
+
+}
+
+buttonEl.addEventListener("click", startQuiz());
+
+openingPage();
