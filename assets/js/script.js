6a68fbc59cec019a63e8a2fc85c3b7411f41834d
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-09-15 16:46:17 -0400

    code works but my buttons are small

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,15 +53,44 @@ function showTimer() {
 function nextQuestion() {
 
     var currentQuestion = questions[index];
-    
+
     console.log(currentQuestion);
 
+    //removes starting page elements from page
     $(displayQuestionEl).text("");
-
+    
+    //makes h3 element = to current question title in the object array.
     $(mainDisplay).text(currentQuestion.title);
 
+    //appends the text we created to the displayquestionsel
     $(displayQuestionEl).append(mainDisplay);
 
+    //creating button for questions
+    var choicesContainer = document.createElement("button")
+
+    //use a loop to: this for loop makes the loop as long as the currentquestion.choices length which is four.
+    for (let i = 0; i < currentQuestion.choices.length; i++) {
+
+        //-create button elements for each choice
+        var choiceBtn = document.createElement("button");
+
+        //add text to each button from question choices
+        $(choiceBtn).text = currentQuestion.choices[i];
+
+        //add a click event listener to button to check answers
+        choiceBtn.addEventListener("click", checkAnswer)
+
+        //append buttons to div element created to wrap choices
+        choicesContainer.append(choiceBtn);
+        
+       
+        
+    }
+
+     //append div element to the question container element
+    $(displayQuestionEl).append(choicesContainer);
+    
+    
 }
 
 function checkAnswer() {
